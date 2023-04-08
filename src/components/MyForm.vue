eb78a9998472599ac79bd52c533fdfce82cbfa78
unknown <kaxudi34801@gmail.com>
2023-04-08 05:33:55 +0500

    create MyForm learn use ref, onMounted

---
+++ src/components/MyForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <p>{{ inputVal }}</p>
+    <form action="" @submit.prevent="">
+        <div>
+            <input type="text" v-model="inputVal">
+        </div>
+        <div>
+            <button>Click</button>
+        </div>
+        
+    </form>
+</template>
+
+<script>
+import {ref} from 'vue'
+
+export default{
+    setup(){
+        let inputVal = ref('');
+
+
+        return {
+            inputVal,
+        }
+    }
+}
+
+</script>
+
+
+<style>
+    div:has(input[type="text"]){
+        margin: 35px;
+    }
+</style>
